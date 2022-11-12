af75542061e9f24e2e8785fd53222de404015a44
HanSungSoo <thepub03@gmail.com>
2022-11-12 12:04:57 +0900

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,6 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>test</h2>
-    ㄴㅁㄴ
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
